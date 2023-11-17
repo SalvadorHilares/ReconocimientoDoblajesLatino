aa01831fef38cdedfa7bcf27e3ae6d21817bb6aa
SalvadorHilares <salvador.hilares@utec.edu.pe>
2023-11-17 15:47:23 -0500

    fetch(fourier):Estructura basica

---
+++ Fourier/Fourier.cpp
@@ -0,0 +1,22 @@
+#include "Fourier.h"
+
+Fourier::Fourier(/* args */)
+{
+}
+
+Fourier::~Fourier()
+{
+}
+
+void Fourier::transformFourier()
+{
+}
+
+
+void Fourier::loadFile()
+{
+}
+
+void Fourier::saveFile()
+{
+}
---
+++ Fourier/Fourier.h
@@ -0,0 +1,16 @@
+#ifndef FOURIER_H
+#define FOURIER_H
+
+class Fourier
+{
+private:
+    /* data */
+public:
+    Fourier(/* args */);
+    void transformFourier();
+    void loadFile();
+    void saveFile();
+    ~Fourier();
+};
+
+#endif
