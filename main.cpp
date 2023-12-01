22336fa7f718887e52666b717fe0f408fcc815cf
RenzoT <renzo.tenazoa@utec.edu.pe>
2023-12-01 00:30:31 -0500

    A ver

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
 #include "kdtree.h"
 
-std::string nameOfNearestNeighbor(std::pair<Point, double> nearestNeighbor) {
-    return nearestNeighbor.first.name;
-}
-
-int main() {
-    int k = 5; // número de vecinos más cercanos a buscar
-
-    //std::vector<Point> points = generateRandomPoints(N, D);
+std::string nameOfNearestNeighbor() {
     std::string filename = "audio_features.csv"; // Cambia esto por la ruta de tu archivo
     std::vector<Point> points = readCSV(filename);
 
@@ -21,22 +14,12 @@ int main() {
     query.name = "Query";
 
     //Print nearest neighbor
-    printVector(query.dimensions);
+    //printVector(query.dimensions);
     auto [nearest, distance] = tree.nearestNeighbor(query);
-    std::cout << nearest.name << std::endl;
-    std::cout << "Nearest neighbor is " << nearest.name << " (";
-    for (const auto& dim : nearest.dimensions) {
-        std::cout << dim << " ";
-    }
-    std::cout << ") at distance " << distance << std::endl;
+    return nearest.name;
+}
 
-    //Print k nearest neighbors
-    std::vector<PointDistancePair> neighbors = tree.kNearestNeighbors(query, k);
-    for (const auto& neighbor : neighbors) {
-        std::cout << "Vecino: " << neighbor.second->point.name << ", Distancia: " << neighbor.first << std::endl;
-    }
-
-    //Print most frequent name
-    std::cout << "El nombre más frecuente es: " << findMostFrequentName(neighbors) << std::endl;
+int main() {
+    std::cout << nameOfNearestNeighbor() << std::endl;
     return 0;
 }
